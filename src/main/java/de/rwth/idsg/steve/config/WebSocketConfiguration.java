ffc71de4978a3033768c28c3e6c0c208da95a656
isalan06 <isalan06@gmail.com>
2022-06-13 10:17:19 +0800

    update

--- src/main/java/de/rwth/idsg/steve/config/WebSocketConfiguration.java
+++ src/main/java/de/rwth/idsg/steve/config/WebSocketConfiguration.java
@@ -73,7 +73,7 @@ public class WebSocketConfiguration implements WebSocketConfigurer {
         handler.setSupportedProtocols(protocols);
 
         for (AbstractWebSocketEndpoint endpoint : endpoints) {
-            registry.addHandler(endpoint, "/echoSocket/*") //"/websocket/CentralSystemService/*")
+            registry.addHandler(endpoint, "/websocket/CentralSystemService/*")
                     .setHandshakeHandler(handler)
                     .setAllowedOrigins("*");
         }
